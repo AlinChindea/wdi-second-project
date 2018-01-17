eeef3e461affbd6bb391bd93dc024cc07579fe11
Alin Chindea <alin.chindea@gmail.com>
2018-01-17 12:20:15 +0000

    js for end and start markers - not fully working yet

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ $(() => {
 
   const $map = $('.map');
   let map    = null;
+  let locations = null;
 
   initMap();
 
@@ -11,9 +12,35 @@ $(() => {
     map = new google.maps.Map($map.get(0), {
       zoom: 14,
       center: latLng
-    });
+      .done((response) => {
+        locations = response;
+        locations.forEach((location) => {
+          addMarker(location);
+        });
+      });
+    },
     const bikeLayer = new google.maps.BicyclingLayer();
     bikeLayer.setMap(map);
+  };
+
+  const startPoint = {
+    lat: $('#startPointLat').val(),
+    lng: $('#startPointLng').val()
+  };
+
+  const endPoint = {
+    lat: $('#endPointLat').val(),
+    lng: $('#endPointLng').val()
+  };
+
+
+  function addMarker(location) {
+    const startLatLng = { lat: startPoint.lat, lng: startPoint.lng };
+    const endLatLng = { lat: endPoint.lat, lng: endPoint.lng};
+    const marker = new google.maps.Marker({
+      position: startLatLng, endLatLng,
+      map: map
+    });
   }
 
   //get the location from the form/model - startPoint and endPoint - separate start lat & lng, and end lat & lng?
