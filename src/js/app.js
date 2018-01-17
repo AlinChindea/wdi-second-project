433cb821e8084225a29896e608fad7b615e7d668
Alin Chindea <alin.chindea@gmail.com>
2018-01-17 16:00:33 +0000

    google api now shows the start and end point of a route

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,43 @@
 /* global google:ignore */
-$(() => {
 
+$(() => {
   const $map = $('.map');
   let map    = null;
 
-
-
   initMap();
 
-
-
   function initMap() {
-    const startPoint = {
-      lat: $('#startPointLat').val(),
-      lng: $('#startPointLng').val()
-    };
-    const endPoint = {
-      lat: $('#endPointLat').val(),
-      lng: $('#endPointLng').val()
-    };
-    const startLatLng = { lat: parseFloat(startPoint.lat), lng: parseFloat(startPoint.lng) };
-    const endLatLng = { lat: parseFloat(endPoint.lat), lng: parseFloat(endPoint.lng)};
+    const markersArray = [];
+    const bounds = new google.maps.LatLngBounds();
 
+    const startPoint = {
+      lat: parseFloat($('#startPointLat').val()),
+      lng: parseFloat($('#startPointLng').val())
+    };
+
+    const endPoint = {
+      lat: parseFloat($('#endPointLat').val()),
+      lng: parseFloat($('#endPointLng').val())
+    };
 
     map = new google.maps.Map($map.get(0), {
-      zoom: 14,
-      center: startLatLng
-    });
-    const marker = new google.maps.Marker({
-      position: startLatLng,
-      position2: endLatLng,
-      map: map
+      zoom: 12,
+      center: startPoint
     });
 
+    createMarker(startPoint);
+    createMarker(endPoint);
+    map.fitBounds(bounds);
+
+    function createMarker(location) {
+      const marker = new google.maps.Marker({
+        position: location,
+        map: map
+      });
+
+      bounds.extend(location);
+    }
     const bikeLayer = new google.maps.BicyclingLayer();
     bikeLayer.setMap(map);
   }
-
-
-
-
-
-  //get the location from the form/model - startPoint and endPoint - separate start lat & lng, and end lat & lng?
-  //add markers and set these to equal the location points
-  //if time allows, add path and enable travelmode - bike to display duration on map
-
-
 });
